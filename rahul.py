7de40b4e734495654120a6a653c82f79893a5c0e
RahulGang1 <rahul7055648508@gmail.com>
2023-03-02 00:46:58 +0530

    first

---
+++ rahul.py
@@ -0,0 +1,43 @@
+# a=["apple","banana","cherry"]
+# a.append("mango")
+# print(a)
+
+# a=["mango","banana","cherry",1,2,3,4,]
+# a.clear()
+# print(a)
+
+# a=["apple","banana","mango"]
+# a.remove("mango")
+# print(a)
+
+# a=["apple","mango","cherry"]
+# a.copy()
+# print(a)
+
+# a=["apple","mango","cherry"]
+# a.insert(1,"orange")
+# print(a)
+
+# a=["apple","mango","cherry"]
+# a.pop(1)
+# print(a)
+
+# a=["apple","mango","cherry"]
+# a.count(a)
+# print(a)
+
+# a=["apple","mango","cherry"]
+# a.remove('mango')
+# print(a)
+
+
+
+
+# a=[1,1,2,4,6,7,8,9,3,4]
+# a.sort()
+# print(a)
+
+
+a=[1,23,45,67,"rahul"]
+a.index(23)
+print(a)
